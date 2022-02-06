94a1d7f9d97a04b32d54cabbd01d6e43ccc6c19b
funzter <cavehive@protonmail.com>
2022-02-06 23:31:06 +0100

    init

---
+++ src/main/java/de/timesnake/extension/discord/main/ExDiscord.java
@@ -0,0 +1,14 @@
+package de.timesnake.extension.discord.main;
+import net.md_5.bungee.api.plugin.Plugin;
+
+
+public class ExDiscord extends Plugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+
+
+}
