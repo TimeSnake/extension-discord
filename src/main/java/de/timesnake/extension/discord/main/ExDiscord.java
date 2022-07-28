13fb8841cc41dede5e64449b9ecea1fb0575f322
funzter <funzter@mail.timesnake.de>
2022-07-28 13:39:25 +0200

    fixed chat colors

--- src/main/java/de/timesnake/extension/discord/main/ExDiscord.java
+++ src/main/java/de/timesnake/extension/discord/main/ExDiscord.java
@@ -1,29 +1,75 @@
 package de.timesnake.extension.discord.main;
 
+import com.google.inject.Inject;
+import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.event.EventManager;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
+import com.velocitypowered.api.plugin.Dependency;
+import com.velocitypowered.api.plugin.PluginManager;
+import com.velocitypowered.api.proxy.ProxyServer;
 import de.timesnake.basic.proxy.util.Network;
 import de.timesnake.extension.discord.util.ConfigFile;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.plugin.Plugin;
-import net.md_5.bungee.api.plugin.PluginManager;
 
 import javax.security.auth.login.LoginException;
+import java.util.logging.Logger;
 
 
-public class ExDiscord extends Plugin {
+@com.velocitypowered.api.plugin.Plugin(id = "extension-proxy", name = "ExProxy", version = "1.0-SNAPSHOT",
+        url = "https://git.timesnake.de", authors = {"MarkusNils"},
+        dependencies = {
+                @Dependency(id = "basic-proxy")
+        })
+public class ExDiscord {
+
+    public static ExDiscord getPlugin() {
+        return plugin;
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public static ProxyServer getServer() {
+        return server;
+    }
+
+    public static EventManager getEventManager() {
+        return server.getEventManager();
+    }
+
+    public static PluginManager getPluginManager() {
+        return server.getPluginManager();
+    }
+
+    public static CommandManager getCommandManager() {
+        return server.getCommandManager();
+    }
 
     private static void registerListeners(JDA api) {
         api.addEventListener(Registration.getInstance());
     }
+
     public static ConfigFile configFile = new ConfigFile();
+    private static ExDiscord plugin;
+    private static ProxyServer server;
+    private static Logger logger;
 
-    @Override
-    public void onEnable() {
+    @Inject
+    public ExDiscord(ProxyServer server, Logger logger) {
+        ExDiscord.server = server;
+        ExDiscord.logger = logger;
+    }
 
-        PluginManager pm = ProxyServer.getInstance().getPluginManager();
-        Network.getCommandHandler().addCommand(this, pm, "discord", Registration.getInstance(),
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        plugin = this;
+
+        Network.getCommandHandler().addCommand(this, "discord", Registration.getInstance(),
                 de.timesnake.extension.discord.main.Plugin.DISCORD);
 
         new ChannelManager();
@@ -38,7 +84,6 @@ public class ExDiscord extends Plugin {
             e.printStackTrace();
         }
 
-
         if (api != null) {
 
             // Init
@@ -52,8 +97,7 @@ public class ExDiscord extends Plugin {
         }
     }
 
-    @Override
-    public void onDisable() {
+    public void onProxyShutdown(ProxyShutdownEvent event) {
         TimeSnakeGuild.getApi().shutdown();
     }
 }
