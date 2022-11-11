10a7764b6108d798237f1d86e758c5b761941415
funzter <cavehive@protonmail.com>
2022-11-11 20:31:15 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/extension/discord/main/Plugin.java
+++ src/main/java/de/timesnake/extension/discord/main/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * extension-discord.main
+ * timesnake.extension-discord.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,12 +18,17 @@
 
 package de.timesnake.extension.discord.main;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.basic.proxy.util.chat.Plugin {
 
-    public static final Plugin DISCORD = new Plugin("Discord", "XDC");
+    public static final Plugin DISCORD = new Plugin("Discord", "XDC", LogHelper.getLogger("Discord", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 
 }
