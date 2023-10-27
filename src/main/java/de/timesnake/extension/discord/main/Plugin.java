4590d7e0ef13f1b799194c25134ba93b32553eb8
funzter <cavehive@protonmail.com>
2023-10-27 23:37:34 +0200

    cleaned up

--- src/main/java/de/timesnake/extension/discord/main/Plugin.java
+++ src/main/java/de/timesnake/extension/discord/main/Plugin.java
@@ -4,17 +4,12 @@
 
 package de.timesnake.extension.discord.main;
 
-import de.timesnake.library.basic.util.LogHelper;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Plugin extends de.timesnake.basic.proxy.util.chat.Plugin {
 
-  public static final Plugin DISCORD = new Plugin("Discord", "XDC",
-      LogHelper.getLogger("Discord", Level.INFO));
+  public static final Plugin DISCORD = new Plugin("Discord", "XDC");
 
-  protected Plugin(String name, String code, Logger logger) {
-    super(name, code, logger);
+  protected Plugin(String name, String code) {
+    super(name, code);
   }
 
 }
