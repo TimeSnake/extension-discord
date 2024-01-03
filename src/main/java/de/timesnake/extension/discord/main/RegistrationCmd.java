0155c43df2efdc861f5e11cb368f1eea5753bc51
funzter <funzter@mail.timesnake.de>
2024-01-04 00:12:24 +0100

    refactored commands

--- src/main/java/de/timesnake/extension/discord/main/RegistrationCmd.java
+++ src/main/java/de/timesnake/extension/discord/main/RegistrationCmd.java
@@ -6,19 +6,16 @@ package de.timesnake.extension.discord.main;
 
 import de.timesnake.basic.proxy.util.Network;
 import de.timesnake.basic.proxy.util.chat.Argument;
+import de.timesnake.basic.proxy.util.chat.CommandListener;
+import de.timesnake.basic.proxy.util.chat.Completion;
 import de.timesnake.basic.proxy.util.chat.Sender;
 import de.timesnake.basic.proxy.util.user.User;
 import de.timesnake.database.util.Database;
 import de.timesnake.library.basic.util.Tuple;
 import de.timesnake.library.chat.ExTextColor;
+import de.timesnake.library.commands.PluginCommand;
+import de.timesnake.library.commands.simple.Arguments;
 import de.timesnake.library.extension.util.chat.Chat;
-import de.timesnake.library.extension.util.cmd.Arguments;
-import de.timesnake.library.extension.util.cmd.CommandListener;
-import de.timesnake.library.extension.util.cmd.ExCommand;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
 import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -27,13 +24,16 @@ import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class RegistrationCmd extends ListenerAdapter implements CommandListener<Sender, Argument> {
+import java.util.HashMap;
+import java.util.Random;
+import java.util.UUID;
+
+public class RegistrationCmd extends ListenerAdapter implements CommandListener {
 
   private final HashMap<UUID, Tuple<UUID, String>> openRegistrationsByUUID = new HashMap<>();
 
   @Override
-  public void onCommand(Sender sender, ExCommand<Sender, Argument> cmd,
-      Arguments<Argument> args) {
+  public void onCommand(Sender sender, PluginCommand cmd, Arguments<Argument> args) {
     if (!args.isLengthLowerEquals(1, true)) {
       return;
     }
@@ -50,8 +50,7 @@ public class RegistrationCmd extends ListenerAdapter implements CommandListener<
       Component component = Chat.getSenderPlugin(Plugin.DISCORD)
           .append(Component.text("Join our discord: ", ExTextColor.PUBLIC))
           .append(Component.text("https://discord.gg/YRCZhFVE9z", ExTextColor.VALUE))
-          .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT,
-              Component.text("Click to open link")))
+          .hoverEvent(HoverEvent.hoverEvent(HoverEvent.Action.SHOW_TEXT, Component.text("Click to open link")))
           .clickEvent(ClickEvent.clickEvent(ClickEvent.Action.OPEN_URL,
               "https://discord.gg/YRCZhFVE9z"));
 
@@ -74,8 +73,7 @@ public class RegistrationCmd extends ListenerAdapter implements CommandListener<
         if (currentDiscordID != null) {
           sender.sendPluginMessage(
               Component.text("Your account is already linked. By continuing, you " +
-                      "will unlink your current discord account.",
-                  ExTextColor.WARNING));
+                  "will unlink your current discord account.", ExTextColor.WARNING));
         }
 
         String code;
@@ -84,8 +82,7 @@ public class RegistrationCmd extends ListenerAdapter implements CommandListener<
         } else {
           Random rnd = new Random();
           code = String.valueOf(rnd.nextInt(100000 - 10000) + 10000);
-          openRegistrationsByUUID.put(user.getUniqueId(),
-              new Tuple<>(user.getUniqueId(), code));
+          openRegistrationsByUUID.put(user.getUniqueId(), new Tuple<>(user.getUniqueId(), code));
         }
 
         Component component = Chat.getSenderPlugin(Plugin.DISCORD)
@@ -134,8 +131,7 @@ public class RegistrationCmd extends ListenerAdapter implements CommandListener<
     } catch (NumberFormatException e) {
       channel.sendMessage(
           "Your message does not have the format of a registration id.\nPlease use the command "
-              +
-              "\"/discord login\" and copy the code by clicking on it.").complete();
+              + "\"/discord login\" and copy the code by clicking on it.").complete();
       return;
     }
 
@@ -145,32 +141,26 @@ public class RegistrationCmd extends ListenerAdapter implements CommandListener<
         long userID = user.getIdLong();
         Database.getUsers().getUser(uuid).setDiscordId(userID);
         openRegistrationsByUUID.remove(uuid);
-        channel.sendMessage(
-            "Your discord account has been successfully linked to your Minecraft account ("
-                + Database.getUsers().getUser(uuid).getName() + ")!").complete();
+        channel.sendMessage("Your discord account has been successfully linked to your Minecraft account ("
+            + Database.getUsers().getUser(uuid).getName() + ")!").complete();
         return;
       }
     }
 
     // Code not found
-    channel.sendMessage(
-        "The entered code does not exist. Please use the command \"/discord login\" and follow "
-            +
-            "the instructions.").complete();
+    channel.sendMessage("The entered code does not exist. Please use the command \"/discord login\" and follow "
+        + "the instructions.").complete();
 
   }
 
   @Override
-  public List<String> getTabCompletion(ExCommand<Sender, Argument> cmd,
-      Arguments<Argument> args) {
-    if (args.length() == 1) {
-      return List.of("login", "help");
-    }
-    return List.of();
+  public Completion getTabCompletion() {
+    return new Completion()
+        .addArgument(new Completion("login", "help"));
   }
 
   @Override
-  public void loadCodes(de.timesnake.library.extension.util.chat.Plugin plugin) {
-
+  public String getPermission() {
+    return null;
   }
 }
